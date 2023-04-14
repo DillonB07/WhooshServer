951526cbd7ff144ce7f2c5a08414441d074d5acf
DillonB07 <dillonb07dev@gmail.com>
2023-04-14 15:37:18 +0000

    Refactor codebase

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/abc", (req, res) => {
+	res.render("index", { title: "Hello World" });
+});
+
+app.listen(3000);
