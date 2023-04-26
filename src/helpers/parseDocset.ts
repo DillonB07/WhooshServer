7a31407ac777f41d01c88b745f456c8037157bdb
DillonB07 <dillonb07dev@gmail.com>
2023-04-26 16:45:40 +0000

    Send files to client

--- src/helpers/parseDocset.ts
+++ src/helpers/parseDocset.ts
@@ -1,53 +1,77 @@
+import path from "path"
 import fs from "fs";
 import { DOMParser } from "xmldom";
+import {getDocsetInfo} from "./xmlUtils"
 
-
-type docsetItem = {
-	name: string;
-	homePage: string;
+export type docsetItemType = {
+    name: string;
+    version: string;
 };
 
-// type remoteDocsetItem = {
-//     name: string;
-//     latestVersion: string
-// }
-
-function getDashIndexFilePath(infoPlist: string): string | undefined {
-	const plist = new DOMParser().parseFromString(infoPlist, "application/xml");
-	const dict = plist.getElementsByTagName("dict")[0];
-	const keys = dict?.getElementsByTagName("key");
-    if (!keys || keys.length === 0) return ;
-	for (let i = 0; i < keys.length; i++) {
-		if (keys[i]?.textContent === "dashIndexFilePath") {
-			const value = keys[i]?.nextSibling?.lastChild?.nodeValue;
-			return value || undefined;
-		}
-	}
-	return undefined;
-}
-
-export async function getAllDocsets(): Promise<docsetItem[] | undefined> {
-	// Get all docsets in the docsets folder
-	const docsets = await fs.promises.readdir("./docsets");
-
-	let docsetList = [];
-	for (const docset of docsets) {
-		// Get the docset name from folder name
-		const docsetName = docset.split(".")[0];
-		// Open info.plist inside docset to get filepath for docs
-		const infoPlist = await fs.promises.readFile(
-			`./docsets/${docset}/Contents/Info.plist`,
-			"utf8"
-		);
-		const dashIndexFilePath = getDashIndexFilePath(infoPlist);
-
-        if (dashIndexFilePath == undefined || docsetName == undefined) {
-            return 
+export function getDashIndexFilePath(infoPlist: string): string | undefined {
+    const plist = new DOMParser().parseFromString(infoPlist, "application/xml");
+    const dict = plist.getElementsByTagName("dict")[0];
+    const keys = dict?.getElementsByTagName("key");
+    if (!keys || keys.length === 0) return;
+    for (let i = 0; i < keys.length; i++) {
+        if (keys[i]?.textContent === "dashIndexFilePath") {
+            const value = keys[i]?.nextSibling?.lastChild?.nodeValue;
+            return value || undefined;
         }
-        
-        docsetList.push({name: docsetName, homePage: dashIndexFilePath})
-	}
-	return docsetList;
+    }
+    return undefined;
 }
 
-// export async function getRemoteDocsets(): Promise<remoteDocsetItem[] | undefined>
+function getDocsetVersionFromPlist(infoPlist: string): string | undefined {
+    const plist = new DOMParser().parseFromString(infoPlist, 'application/xml');
+    const dict = plist.getElementsByTagName('dict')[0]
+    const keys = dict?.getElementsByTagName('key');
+    if (!keys || keys.length === 0) return;
+    for (let i = 0; i < keys.length; i ++) {
+        if (keys[i]?.textContent == "version") {
+            const value = keys[i]?.nextSibling?.textContent;
+            return value || undefined
+        }
+    }
+    return undefined
+}
+
+export async function getAllLocalDocsets(): Promise<docsetItemType[] | undefined> {
+    // Get all docsets in the docsets folder
+    const docsets = await fs.promises.readdir("./docsets");
+
+    let docsetList = [];
+    for (const docset of docsets) {
+        // Get the docset name from folder name
+        const docsetName = docset.split(".")[0];
+        // Open info.plist inside docset to get filepath for docs
+        const infoPlist = await fs.promises.readFile(
+            `./docsets/${docset}/Contents/Info.plist`,
+            "utf8"
+        );
+        const version = getDocsetVersionFromPlist(infoPlist);
+
+        if (version == undefined || docsetName == undefined) {
+            return
+        }
+
+        docsetList.push({ name: docsetName, version })
+    }
+    return docsetList;
+}
+
+export async function getAllDocsets(): Promise<docsetItemType[]> {
+    let docsetList: docsetItemType[] = []
+    const xmlFolder = './feeds'
+    const files = fs.readdirSync(xmlFolder)
+
+    for (const file of files) {
+      if (file.endsWith('.xml')) {
+          // Parse the XML file
+          const data = await getDocsetInfo(path.join(xmlFolder, file))
+          docsetList.push(data)
+      }
+    }
+    return docsetList
+}
+
--- src/helpers/xmlUtils.ts
+++ src/helpers/xmlUtils.ts
@@ -2,81 +2,91 @@ import https from "https";
 import fs from "fs";
 import path from "path";
 import { parseStringPromise } from "xml2js";
+import type {docsetItemType} from './parseDocset'
 
-type docsetType = {
-	success: boolean;
-	message: string;
-	docsetPath: string | undefined;
+export type docsetType = {
+    success: boolean;
+    message: string;
+    docsetPath: string | undefined;
 };
 
 async function downloadFile(
-	url: string,
-	folder: string
+    url: string,
+    folder: string
 ): Promise<string | undefined> {
-	if (!url) return;
+    if (!url) return;
 
-	if (!folder) return;
+    if (!folder) return;
 
-	let docsetFilename = url.split("/").pop() as string;
-	let docsetFilepath = path.join(folder, docsetFilename);
-	if (fs.existsSync(docsetFilepath)) {
-		// Add unique suffix to filename if it already exists
-		let i = 1;
-		while (
-			fs.existsSync(
-				path.join(folder, `${path.parse(docsetFilename).name}_${i}.tgz`)
-			)
-		) {
-			i++;
-		}
-		docsetFilename = `${path.parse(docsetFilename).name}_${i}.tgz`;
-		docsetFilepath = path.join(folder, docsetFilename);
-	}
-	return new Promise<string>((resolve, reject) => {
-		const file = fs.createWriteStream(docsetFilepath);
-		https
-			.get(url, (response) => {
-				response.pipe(file);
-				file.on("finish", () => {
-					file.close();
-					console.log(`Downloaded ${docsetFilename}`);
-					resolve(docsetFilepath);
-				});
-			})
-			.on("error", (err) => {
-				fs.unlink(docsetFilepath, () => {});
-				console.error(`Error downloading ${docsetFilename}: ${err}`);
-				reject(err);
-			});
-	});
+    let docsetFilename = url.split("/").pop() as string;
+    let docsetFilepath = path.join(folder, docsetFilename);
+    if (fs.existsSync(docsetFilepath)) {
+        // Add unique suffix to filename if it already exists
+        let i = 1;
+        while (
+            fs.existsSync(
+                path.join(folder, `${path.parse(docsetFilename).name}_${i}.tgz`)
+            )
+        ) {
+            i++;
+        }
+        docsetFilename = `${path.parse(docsetFilename).name}_${i}.tgz`;
+        docsetFilepath = path.join(folder, docsetFilename);
+    }
+    return new Promise<string>((resolve, reject) => {
+        const file = fs.createWriteStream(docsetFilepath);
+        https
+            .get(url, (response) => {
+                response.pipe(file);
+                file.on("finish", () => {
+                    file.close();
+                    console.log(`Downloaded ${docsetFilename}`);
+                    resolve(docsetFilepath);
+                });
+            })
+            .on("error", (err) => {
+                fs.unlink(docsetFilepath, () => { });
+                console.error(`Error downloading ${docsetFilename}: ${err}`);
+                reject(err);
+            });
+    });
 }
 
-async function downloadDocset(xmlFilePath: string): Promise<docsetType> {
-	// Parse the XML file
-	const xml = await fs.promises.readFile(xmlFilePath);
-	try {
-		const result = await parseStringPromise(xml);
-		const root = result.entry;
-		const docsetFile = await downloadFile(root.url[1], "./docsets");
-		if (docsetFile) {
-			// File downloaded successfully
-			return {
-				success: true,
-				message: "Docset successfully downloaded",
-				docsetPath: docsetFile,
-			};
-		} else {
-			// File not downloaded
-			return {
-				success: false,
-				message: "Docset not found",
-				docsetPath: undefined,
-			};
-		}
-	} catch (err) {
-		// Error parsing XML or downloading file.
-		return { success: false, message: "Invalid XML", docsetPath: undefined };
-	}
+export async function downloadDocset(xmlFilePath: string): Promise<docsetType> {
+    // Parse the XML file
+    const xml = await fs.promises.readFile(xmlFilePath);
+    try {
+        const result = await parseStringPromise(xml);
+        const root = result.entry;
+        const docsetFile = await downloadFile(root.url[1], "./docsets");
+        if (docsetFile) {
+            // File downloaded successfully
+            return {
+                success: true,
+                message: "Docset successfully downloaded",
+                docsetPath: docsetFile,
+            };
+        } else {
+            // File not downloaded
+            return {
+                success: false,
+                message: "Docset not found",
+                docsetPath: undefined,
+            };
+        }
+    } catch (err) {
+        // Error parsing XML or downloading file.
+        return { success: false, message: "Invalid XML", docsetPath: undefined };
+    }
 }
 
-export { downloadDocset };
+
+export async function getDocsetInfo(xmlFilePath: string): Promise<docsetItemType> {
+    const file = fs.readFileSync(xmlFilePath)
+    const result = await parseStringPromise(file)
+    
+    const version = result.entry?.version?.toString()
+    const name = path.basename(xmlFilePath, '.xml')
+    return {name, version}
+}
+
